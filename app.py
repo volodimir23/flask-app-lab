4b3a70cd449f95b383d476eeb083b7179b2d663a
володимир23 <volodimirzidun@gmail.com>
2024-11-06 11:46:06 +0200

    Staged Changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -6,21 +6,8 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-from flask import Flask, request, redirect, url_for
-
-app = Flask(__name__)
-
-
-
-@app.route('/')
-def main():
-    return 'Hello, World!'
-
 @app.route('/homepage')
-def home():
+def homepage():
     """View for the Home page of your website."""
     agent = request.user_agent
     return f"<h1>This is your homepage :) - {agent}</h1>"
@@ -36,17 +23,10 @@ def admin():
     to_url = url_for("greetings", name="administrator", _external=True)
     return redirect(to_url)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-    from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
+@app.route('/resume')
+def resume():
     return render_template('resume.html', title='Резюме')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
